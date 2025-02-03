d56e58f5390d4f091cd513ab84f21ebf9ecdaa63
Tachera Sasi <tacherasasi@gmail.com>
2025-02-03 12:44:25 +0300

    added the floating path bg in the auth pages

--- web/app/sign-in/[[...sign-in]]/page.tsx
+++ web/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,13 @@
+import { FloatingPaths } from "@/components/floating-paths";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="flex h-screen items-center justify-center">
+      <div className="absolute inset-0">
+        <FloatingPaths position={1} />
+        <FloatingPaths position={-1} />
+      </div>
       <SignIn />
     </div>
   );
