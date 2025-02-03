07ab980f30d8e919fec513d6ea888e7e14823660
Tachera Sasi <tacherasasi@gmail.com>
2025-02-03 21:40:01 +0300

    more changes

--- web/app/sign-in/[[...sign-in]]/page.tsx
+++ web/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,7 @@
 import { FloatingPaths } from "@/components/floating-paths";
 import { SignIn } from "@clerk/nextjs";
 
+
 export default function Page() {
   return (
     <div className="flex h-screen items-center justify-center">
