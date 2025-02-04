0687734ce058d14d6d8a278dc20fee6fdada31e9
Tachera Sasi <tacherasasi@gmail.com>
2025-02-04 19:02:32 +0300

    remove the github auth

--- web/src/features/auth/components/user-auth-form.tsx
+++ web/src/features/auth/components/user-auth-form.tsx
@@ -77,17 +77,7 @@ export default function UserAuthForm() {
           </Button>
         </form>
       </Form>
-      <div className='relative'>
-        <div className='absolute inset-0 flex items-center'>
-          <span className='w-full border-t' />
-        </div>
-        <div className='relative flex justify-center text-xs uppercase'>
-          <span className='bg-background px-2 text-muted-foreground'>
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <GithubSignInButton />
+      
     </>
   );
 }
