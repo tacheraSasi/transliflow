8f71098312335cf3f4efccd2e5ec38b09fbe50a9
Tachera Sasi <tacherasasi@gmail.com>
2025-02-04 18:57:57 +0300

    more changes to the dashboard page

--- web/src/app/sign-in/page.tsx
+++ web/src/app/sign-in/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from 'next';
 import SignInViewPage from '@/features/auth/components/sigin-view';
 
 export const metadata: Metadata = {
-  title: 'Authentication | Sign In',
-  description: 'Sign In page for authentication.'
+  title: 'Trasliflow | Sign In',
+  description: 'Sign In to Transliflow'
 };
 
 export default async function Page() {
--- web/src/features/overview/components/overview.tsx
+++ web/src/features/overview/components/overview.tsx
@@ -136,28 +136,7 @@ export default function OverViewPage() {
                 </CardContent>
               </Card>
             </div>
-            <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7'>
-              <div className='col-span-4'>
-                <BarGraph />
-              </div>
-              <Card className='col-span-4 md:col-span-3'>
-                <CardHeader>
-                  <CardTitle>Recent Sales</CardTitle>
-                  <CardDescription>
-                    You made 265 sales this month.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <RecentSales />
-                </CardContent>
-              </Card>
-              <div className='col-span-4'>
-                <AreaGraph />
-              </div>
-              <div className='col-span-4 md:col-span-3'>
-                <PieGraph />
-              </div>
-            </div>
+            
           </TabsContent>
         </Tabs>
       </div>
