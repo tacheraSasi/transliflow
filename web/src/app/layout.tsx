fcefc94004b8b6f7e7af51c7f20363323aac8fb8
Tachera Sasi <tacherasasi@gmail.com>
2025-02-04 18:55:48 +0300

    more changes

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({
   const session = await auth();
   return (
     <html lang='en' className={`${lato.className}`} suppressHydrationWarning>
-      <body className={'overflow-hidden'}>
+      <body className={'overflow-hidden bg-gradient-to-b from-neutral-100 to-white dark:from-neutral-900 dark:to-black'}>
         <NextTopLoader showSpinner={false} />
         <NuqsAdapter>
           <Providers session={session}>
