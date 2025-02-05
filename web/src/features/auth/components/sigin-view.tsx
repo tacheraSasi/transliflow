a04e047776fee0357fe5ec05df0c63ec4c740dea
Tachera Sasi <tacherasasi@gmail.com>
2025-02-05 13:55:48 +0300

    more changes added the svg icon

--- web/src/features/auth/components/sigin-view.tsx
+++ web/src/features/auth/components/sigin-view.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import UserAuthForm from './user-auth-form';
+import Icon from '@/components/Icon';
 
 export const metadata: Metadata = {
   title: 'Authentication',
@@ -24,19 +25,8 @@ export default function SignInViewPage() {
       <div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex'>
         <div className='absolute inset-0 bg-zinc-900' />
         <div className='relative z-20 flex items-center text-lg font-medium'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            className='mr-2 h-6 w-6'
-          >
-            <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-          </svg>
-          Logo
+          <Icon/>
+          Transliflow
         </div>
         <div className='relative z-20 mt-auto'>
           <blockquote className='space-y-2'>
