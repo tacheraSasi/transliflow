b8e2037ba917a6e6d5a518984e71c97b9d9b294c
Tachera Sasi <tacherasasi@gmail.com>
2025-02-07 11:32:08 +0300

    more changes remove the tiptap bs

--- src/app/dashboard/translate/page.tsx
+++ src/app/dashboard/translate/page.tsx
@@ -1,13 +1,19 @@
-import PageContainer from "@/components/layout/page-container";
-import { Textarea } from "@/components/ui/textarea";
+import PageContainer from '@/components/layout/page-container';
+import { Textarea } from '@/components/ui/textarea';
 
-export default function Page(){
+export default function Page() {
   return (
     <PageContainer>
-      <div className="flex w-full gap-2">
-        <Textarea className="flex-1 w-full h-full "/>
-        <Textarea className="flex-1 w-full h-full "/>
+      <div className='flex w-full gap-2'>
+        <div className='h-full w-full flex-1'>
+          <p>Original</p>
+          <Textarea className='h-full w-full' />
+        </div>
+        <div className='h-full w-full flex-1'>
+          <p>Translated</p>
+          <Textarea className='h-full w-full' />
+        </div>
       </div>
     </PageContainer>
-  )
+  );
 }
---
+++ src/components/TiptapEditor.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: '<p>Hello World! 🌎️</p>',
+  })
+
+  return <EditorContent editor={editor} className='h-full' />
+}
+
+export default Tiptap
