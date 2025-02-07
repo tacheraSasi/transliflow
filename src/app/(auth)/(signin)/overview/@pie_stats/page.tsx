ae7fc51bb8fb541b5b589b2e463079fb4f9b01af
Revaycolizer <albinuslucas@gmail.com>
2025-02-07 18:11:55 +0300

    Removed Auth Page
    Changes to be committed:
    	deleted:    src/app/(auth)/(signin)/overview/@area_stats/error.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@area_stats/loading.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@area_stats/page.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@bar_stats/error.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@bar_stats/loading.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@bar_stats/page.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@pie_stats/error.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@pie_stats/loading.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@pie_stats/page.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@sales/error.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@sales/loading.tsx
    	deleted:    src/app/(auth)/(signin)/overview/@sales/page.tsx
    	deleted:    src/app/(auth)/(signin)/overview/error.tsx
    	deleted:    src/app/(auth)/(signin)/overview/layout.tsx
    	deleted:    src/app/(auth)/(signin)/page.tsx

--- src/app/(auth)/(signin)/overview/@pie_stats/page.tsx
+++
@@ -1,7 +0,0 @@
-import { delay } from '@/constants/mock-api';
-import { PieGraph } from '@/features/overview/components/pie-graph';
-export const revalidate = 0;
-export default async function Stats() {
-  await delay(1000);
-  return <PieGraph />;
-}
